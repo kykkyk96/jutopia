b606893100d5d871aeb37847fbfede96261aa76c
youkyung Kim <beastium96@naver.com>
2024-12-26 14:50:27 +0900

    first commit

---
+++ src/main/java/com/jtp/jutopia/JutopiaApplication.java
@@ -0,0 +1,13 @@
+package com.jtp.jutopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JutopiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JutopiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jtp/jutopia/JutopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtp.jutopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JutopiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
