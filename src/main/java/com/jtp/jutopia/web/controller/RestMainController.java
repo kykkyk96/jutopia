f1fc17275930b57402de6f6716fa691825d495eb
youkyung Kim <beastium96@naver.com>
2025-02-24 15:50:51 +0900

    오류해결좀..

--- src/main/java/com/jtp/jutopia/web/controller/RestMainController.java
+++ src/main/java/com/jtp/jutopia/web/controller/RestMainController.java
@@ -1,15 +1,15 @@
 package com.jtp.jutopia.web.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-@RestController
+@Controller
 public class RestMainController {
     
-    @GetMapping("/api/data")
+    @GetMapping("/index")
     public String test() {
-        return "hello";
+        return "index";
     }
     
 }
